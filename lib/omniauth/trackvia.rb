a17d2d3bb1adac08ce2a152df57fd8e6cb0224f9
Pavel Perkin <pavlo.perkin@workato.com>
2019-04-29 15:22:47 +0300

    Init gem with Bundler

---
+++ lib/omniauth/trackvia.rb
@@ -0,0 +1,8 @@
+require "omniauth/trackvia/version"
+
+module Omniauth
+  module Trackvia
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/trackvia/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Trackvia
+    VERSION = "0.1.0"
+  end
+end
